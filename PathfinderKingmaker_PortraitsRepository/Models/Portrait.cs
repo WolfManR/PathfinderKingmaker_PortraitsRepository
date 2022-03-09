7518e13545bee6fdfe6109443310a9dd5ff63242
Tarxos <ftarxos@gmail.com>
2022-03-10 02:20:23 +0300

    add portrait model and portraits repository

---
+++ PathfinderKingmaker_PortraitsRepository/Models/Portrait.cs
@@ -0,0 +1,15 @@
+﻿using System.Reactive;
+using Avalonia.Media.Imaging;
+using ReactiveUI;
+
+namespace PathfinderKingmaker_PortraitsRepository.Models;
+
+public sealed class Portrait
+{
+    public const string MediumImageName = "Medium.png";
+    public string Name { get; set; }
+    public string DirPath { get; set; }
+    public Bitmap MediumImage { get; set; }
+
+    public ReactiveCommand<Portrait, Unit> DeleteCommand { get; set; }
+}
---
+++ PathfinderKingmaker_PortraitsRepository/Models/PortraitsRepository.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System;
+using System.Linq;
+
+namespace PathfinderKingmaker_PortraitsRepository.Models;
+
+public sealed class PortraitsRepository
+{
+    private readonly string _baseUrl = @"Owlcat Games\Pathfinder Kingmaker\Portraits";
+
+    public IEnumerable<Portrait> GetImages()
+    {
+        DirectoryInfo dir = new(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "Low", _baseUrl));
+        foreach (var portraitDir in dir.EnumerateDirectories())
+        {
+            Portrait portrait = new()
+            {
+                Name = portraitDir.Name,
+                DirPath = portraitDir.FullName
+            };
+
+            var files = portraitDir.GetFiles().ToList();
+
+            if (files.FirstOrDefault(f => f.Name == Portrait.MediumImageName) is { } mediumImage)
+            {
+                portrait.MediumImage = new(mediumImage.FullName);
+                files.Remove(mediumImage);
+            }
+
+            yield return portrait;
+        }
+    }
+
+    public bool DeletePortrait(Portrait portrait)
+    {
+        if (Directory.Exists(portrait.DirPath))
+        {
+            try
+            {
+                Directory.Delete(portrait.DirPath, true);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        return false;
+    }
+}
