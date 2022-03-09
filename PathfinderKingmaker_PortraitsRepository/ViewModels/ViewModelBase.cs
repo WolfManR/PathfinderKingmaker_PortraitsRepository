399e348d938abb72096366a434ba1e8603a35d39
Tarxos <ftarxos@gmail.com>
2022-03-10 02:17:03 +0300

    Add project files.

---
+++ PathfinderKingmaker_PortraitsRepository/ViewModels/ViewModelBase.cs
@@ -0,0 +1,12 @@
+using ReactiveUI;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PathfinderKingmaker_PortraitsRepository.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ PathfinderKingmaker_PortraitsRepository/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace PathfinderKingmaker_PortraitsRepository.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
