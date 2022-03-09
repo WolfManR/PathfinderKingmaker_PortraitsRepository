13977560277306ceda82f295bc6e2e20b20073c0
Tarxos <ftarxos@gmail.com>
2022-03-10 02:21:58 +0300

    clear using directives

--- PathfinderKingmaker_PortraitsRepository/ViewModels/ViewModelBase.cs
+++ PathfinderKingmaker_PortraitsRepository/ViewModels/ViewModelBase.cs
@@ -1,9 +1,5 @@
 using ReactiveUI;
 
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace PathfinderKingmaker_PortraitsRepository.ViewModels
 {
     public class ViewModelBase : ReactiveObject
